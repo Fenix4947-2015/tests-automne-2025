7f7b53460b72bb13a84c5fc334e9a37be1329257
etdube <etdube@gmail.com>
2025-11-16 20:53:33 -0500

    simplification (garder seulement drivetrain et winch)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,65 +19,11 @@ public final class Constants {
     }
 
     public static class ElectricConstants {
-        //public static final int kArmMotor1CanId = 5;
-        public static final int kArmMotor1CanId = 6;
-        public static final int kArmMotor2CanId = 7;
-        public static final int kArmMotor3CanId = 8;
-        public static final int kArmCancoderCanId = 9;
-        public static final int kArmLowLimitSwitchChannel = 8;
-        public static final int kArmGripperProximityChannel = 4;
-        public static final int kArmSideGripperLimitSwitch = 7;
-        public static final int kArmFrontGripperLimitSwitch = 6;
-        public static final int kArmExtenderChannel = 4;
-        public static final int kArmSideGripperChannel = 15; //15; //0
-        public static final int kArmFrontGripperChannel = 2;
-        public static final int kArmPusherChannel = 11;
-
-        public static final int kBallsCanId = 12;
-
-        public static final int kWinchCageGripperChannel = 15;
         public static final int kWinchMotorOneChannel = 16;
         public static final int kWinchMotorTwoChannel = 17;
         public static final int kWinchEncoderChannel1 = 0;
         public static final int kWinchEncoderChannel2 = 1;
 
         public static final int kPigeon2CanId = 3;
-        public static final int kPneumaticHubCanId = 2;
-    }
-
-    public static class Arm {
-        public static final double kP = 7.5;
-        public static final double kI = 0;
-        public static final double kD = 0;
-        public static final double kS = 0.01;
-        public static final double kG = 0.025;
-        public static final double kV = 0;
-        public static final double kToleranceDistance = 0.01;
-        public static final double kLowestPosition = 0.215;
-        public static final double kLimitOutputUntilPosition = 0.394;
-        public static final double applyDownwardStabilizingPosition = 0.25;
-        public static final double kHighestPosition = 1.25;
-        public static final double kCoralL4Position = 1.09;
-        public static final double kCoralL3Position = 0.755;
-        public static final double kCoralL2Position = 0.650;
-        public static final double lowestRetractedPosition = 0.640;
-        public static final double gripperDangerZonePosition = 0.3;
-        public static final double horizontalPosition = 0.752;
-        public static final double encoderToArmGearRatio = 12.0 / 36.0;
-        public static final double kDownwardStablizingOutput = -0.025;
-    }
-
-    public static class Limelight {
-        public static final double offsetX = -0.46;
-        public static final double offsetY = 0;
-        public static final double offsetZ = 0.43;
-        public static final double angleCamera = 60 - 15;
-
-    }
-
-    public static class Drivetrain {
-        public static final double PATHPLANNER_TOLERANCE_X = 0.02;
-        public static final double PATHPLANNER_TOLERANCE_Y = 0.02;
-        public static final double PATHPLANNER_TOLERANCE_ANGLE = 0.02;
     }
 }
